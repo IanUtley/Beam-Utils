f28d1914595ddca4a2f9b4bbf0ceb15b19c1204c
Ian Utley <utley.ian+git@gmail.com>
2017-12-19 17:55:43 +0000

    Added test example

---
+++ beam-examples/src/main/java/org/beam/examples/StarterPipeline.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.beam.examples;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.Read;
+import org.apache.beam.sdk.options.Description;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.options.ValueProvider;
+import org.apache.beam.sdk.options.ValueProvider.StaticValueProvider;
+import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.SimpleFunction;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TypeDescriptor;
+import org.ianutley.beam.DelimitedTextSource;
+import org.ianutley.beam.TextRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A starter example for writing Beam programs.
+ *
+ * <p>The example takes two strings, converts them to their upper-case
+ * representation and logs them.
+ *
+ * <p>To run this starter example locally using DirectRunner, just
+ * execute it without any additional parameters from your favorite development
+ * environment.
+ *
+ * <p>To run this starter example using managed resource in Google Cloud
+ * Platform, you should specify the following command-line options:
+ *   --project=<YOUR_PROJECT_ID>
+ *   --stagingLocation=<STAGING_LOCATION_IN_CLOUD_STORAGE>
+ *   --runner=DataflowRunner
+ */
+public class StarterPipeline {
+  private static final Logger LOG = LoggerFactory.getLogger(StarterPipeline.class);
+
+  public interface MyOptions extends PipelineOptions {
+      @Description("Input file")
+      public ValueProvider<String> getInputFile();
+      void setInputFile(ValueProvider<String> file);
+  }
+  
+  public static void main(String[] args) {
+      MyOptions options = PipelineOptionsFactory.fromArgs(args).withValidation().as(MyOptions.class);
+      Pipeline p = Pipeline.create(options);
+
+      PCollection<TextRecord> data = p.apply("Read CSV",
+          Read.from(DelimitedTextSource.withFilename(options.getInputFile())));
+
+      data.apply(Count.globally()).apply("Result", MapElements.into(TypeDescriptor.of(Long.class)).via((Long l) -> {
+          LOG.info(l.toString());
+          return l;
+      }));
+
+    p.run();
+  }
+}
